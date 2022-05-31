e41b00d4890ccf6cc6115cd6ff869907afef0ee3
Jason <jlee220@gmail.com>
2022-05-30 23:03:22 -0700

    add sign up page

--- ghi/case-n-part/src/App.js
+++ ghi/case-n-part/src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./Nav";
 import Login from "./login/Login";
 import SignUp from "./login/SignUp";
-
+import HomePage from "./HomePage";
 function App() {
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<SignUp />} />
         </Routes>
--- ghi/case-n-part/src/Nav.js
+++ ghi/case-n-part/src/Nav.js
@@ -3,7 +3,7 @@ import { Button, Navbar } from "./static/cnpStyle";
 import logo from "./static/logo.png";
 
 const links = [
-  { name: "My Builds", path: "/" },
+  { name: "My Builds", path: "/mybuilds" },
   { name: "Create Builds", path: "/create" },
   { name: "View Builds", path: "/toprated" },
   { name: "Login", path: "/login" },
@@ -18,7 +18,9 @@ function Nav() {
         </NavLink>
       ))}
       <div>
-        <img src={logo} alt="Case N Parts" width="350" />
+        <NavLink to="/">
+          <img src={logo} alt="Case N Parts" width="350" />
+        </NavLink>
       </div>
     </Navbar>
   );
