e443032c237ac5478342e32b3fc8079f5d692764
chad.szymarek <chadszymarek@gmail.com>
2022-06-02 13:38:55 -0600

    fixed typos in schema

---
+++ data/02-create-build-schema copy.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS public.build
+(
+    Id SERIAL NOT NULL PRIMARY KEY,
+    "Name" CHARACTER VARYING(200) NOT NULL,
+    MoboId INTEGER REFERENCES mobos,
+    CpuId INTEGER REFERENCES cpu,
+    PsuId INTEGER REFERENCES psu ,
+    "Private" BOOLEAN NOT NULL DEFAULT false,
+
+    UNIQUE(CpuId, PsuId, MoboId)
+);
