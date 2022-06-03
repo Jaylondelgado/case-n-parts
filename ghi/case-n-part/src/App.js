453fb65c6963f0d099cd7be307cfbeacb2a01487
Jason <jlee220@gmail.com>
2022-06-03 11:15:28 -0700

    added pyclean to req file

--- ghi/case-n-part/src/App.js
+++ ghi/case-n-part/src/App.js
@@ -6,12 +6,9 @@ import Login from "./login/Login";
 import SignUp from "./login/SignUp";
 import HomePage from "./HomePage";
 import MyBuilds from "./builds/MyBuilds";
-<<<<<<< HEAD
 import HddList from "./builds/PcPartsFetch";
-=======
 import MoboList from "./builds/MoboFetch";
 import RamList from "./builds/RamFetch";
->>>>>>> 0703479203a00723e57243fd4c295957bb6a263c
 
 function App() {
   return (
@@ -19,20 +16,13 @@ function App() {
       <Nav />
       <div className="container">
         <Routes>
-<<<<<<< HEAD
           <Route path="/" element={<HomePage />} />
           <Route path="mybuilds" element={<MyBuilds />} />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<SignUp />} />
-          <Route path="parts" element={<HddList />} />
-=======
-          <Route path='/' element={<HomePage />} />
-          <Route path='mybuilds' element={<MyBuilds />} />
-          <Route path='login' element={<Login />} />
-          <Route path='signup' element={<SignUp />} />
-          <Route path='mobos' element={<MoboList />} />
-          <Route path='rams' element={<RamList />} />
->>>>>>> 0703479203a00723e57243fd4c295957bb6a263c
+          <Route path="mobos" element={<MoboList />} />
+          <Route path="hdds" element={<HddList />} />
+          <Route path="rams" element={<RamList />} />
         </Routes>
       </div>
       <Footer />
