9c34eea702cde8453e50618f626e092f2d4c3d5d
Jaylon Delgado <jaylondelgado1124@gmail.com>
2022-06-03 10:41:16 -0700

    ram and mobo data to front end

---
+++ ghi/case-n-part/src/builds/MoboFetch.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+function MoboList() {
+  const [mobos, setMobo] = useState([]);
+
+  useEffect(() => {
+    const getMoboData = async () => {
+      const moboResponse = await fetch("http://localhost:8000/api/mobos/");
+      const moboData = await moboResponse.json();
+      setMobo(moboData.mobos);
+    };
+
+    getMoboData();
+  }, []);
+
+  return (
+    <select>
+      <option value="">Select your Motherboard</option>
+      {mobos.map((mobo) => {
+        return (
+          <option key={mobo.id} value={mobo.id}>
+            {mobo.socket_type} {mobo.max_memory} {mobo.max_memory_per_slot} {ram.pcie_slots} {ram.memory_slots}
+          </option>
+        );
+      })}
+    </select>
+  );
+}
+
+export default MoboList;
