73fe9f61cfbb01a7e8c14c6feeac5bef7ef27af7
Jason <jlee220@gmail.com>
2022-06-07 16:48:24 -0700

    testing

--- ghi/case-n-part/src/builds/ViewBuilds.js
+++ ghi/case-n-part/src/builds/ViewBuilds.js
@@ -1,15 +1,33 @@
-import React from "react";
-import { Grid, Cell } from "styled-css-grid";
+// import React, { useState, useEffect } from "react";
+// import { Grid, Cell } from "styled-css-grid";
 
-function ViewBuilds() {
-  return (
-    <Grid columns={"50px 50px 50px"} justifyContent="space-evenly">
-      <Cell>
-        <img src={"src/static/black-large.jpg"} />
-      </Cell>
-      <Cell>B</Cell>
-      <Cell>C</Cell>
-    </Grid>
-  );
-}
-export default ViewBuilds;
+// function ViewBuildsList() {
+//   const [viewBuilds, SetViewBuilds] = useState([]);
+
+//   useEffect(() => {
+//     const getCpuData = async () => {
+//       const cpuResponse = await fetch("http://localhost:8000/api/caseimage/");
+//       const cpuData = await cpuResponse.json();
+//       setCpu(cpuData.cpus);
+//     };
+
+//     getCpuData();
+//   }, []);
+
+//   return cpus;
+// }
+
+// export default CpuList;
+
+// function ViewBuilds() {
+//   return (
+//     <Grid columns={"50px 50px 50px"} justifyContent="space-evenly">
+//       <Cell>
+//         <img src={"src/static/black-large.jpg"} />
+//       </Cell>
+//       <Cell>B</Cell>
+//       <Cell>C</Cell>
+//     </Grid>
+//   );
+// }
+// export default ViewBuilds;
