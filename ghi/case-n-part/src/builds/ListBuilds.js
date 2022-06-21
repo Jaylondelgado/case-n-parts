b0e8f5cb7fbfcce0dce66528c83a1840ea9e46d2
Jason <jlee220@gmail.com>
2022-06-20 22:13:44 -0700

    added pagination to my build page

--- ghi/case-n-part/src/builds/ListBuilds.js
+++ ghi/case-n-part/src/builds/ListBuilds.js
@@ -25,31 +25,25 @@ function ListBuilds() {
     .map((build) => {
       if (builds.length > 0) {
         return (
-          <>
-            <div className="row py-3 justify-content-center" key={build.id}>
-              <div className="card h-100 border-light bg-transparent">
-                <img
-                  src={build.picture}
-                  className="card-img-top p-3"
-                  alt="..."
-                />
-                <div className="card-body">
-                  <h5 className="card-title text-info">{build.Name}</h5>
-                  <p className="card-text text-primary">{build.username}</p>
-                  <Link
-                    to={`/builds/detailbuild/${build.id}`}
-                    className="btn btn-outline-primary"
-                  >
-                    Build Detail
-                  </Link>
-                  <div className="btn btn-primary  mx-1">
-                    <i className="bi bi-hand-thumbs-up"></i>
-                    {build.likes}
-                  </div>
+          <div className="row py-3 justify-content-center" key={build.id}>
+            <div className="card h-100 border-light bg-transparent">
+              <img src={build.picture} className="card-img-top p-3" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title text-info">{build.Name}</h5>
+                <p className="card-text text-primary">{build.username}</p>
+                <Link
+                  to={`/builds/detailbuild/${build.id}`}
+                  className="btn btn-outline-primary"
+                >
+                  Build Detail
+                </Link>
+                <div className="btn btn-primary  mx-1">
+                  <i className="bi bi-hand-thumbs-up"></i>
+                  {build.likes}
                 </div>
               </div>
             </div>
-          </>
+          </div>
         );
       } else {
         return (
--- ghi/case-n-part/src/builds/MyBuilds.js
+++ ghi/case-n-part/src/builds/MyBuilds.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "../index.css";
+import ReactPaginate from "react-paginate";
 
 function MyBuilds(props) {
   const [builds, setBuild] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
 
   useEffect(() => {
     const getBuildData = async () => {
@@ -30,50 +33,80 @@ function MyBuilds(props) {
     window.location.reload();
   };
 
-  return (
-    <div className="container justify-content-center pt-5 my-5">
-      {builds.length > 0 && (
-        <div className="row">
-          {builds.map((build) => {
-            return (
-              <div key={build.id} className="col-sm m-3">
-                <div className="card h-100 border-light bg-transparent">
-                  <img
-                    src={build.picture}
-                    className="card-img-top p-3"
-                    alt="..."
-                  />
-                  <div className="card-body">
-                    {console.log(build.name)}
-                    <h5 className="card-title text-info">{build.Name}</h5>
-                    <p className="card-text text-primary">
-                      {build.gpu.chipset}
-                    </p>
-                    <Link
-                      to={`/builds/detailbuild/${build.id}`}
-                      className="btn btn-outline-primary"
-                    >
-                      Build Detail
-                    </Link>
-                    <div style={{ color: "lightblue" }} className="pt-3">
-                      <i
-                        type="button"
-                        onClick={() => {
-                          deleteBuild(build.id);
-                        }}
-                        className="fa fa-trash fa-xl"
-                        aria-hidden="true"
-                      ></i>
-                    </div>
+  const buildsPerPage = 6;
+  const pagesVisited = pageNumber * buildsPerPage;
+
+  const displayBuilds = builds
+    .slice(pagesVisited, pagesVisited + buildsPerPage)
+    .map((build) => {
+      if (builds.length > 0) {
+        return (
+          <>
+            <div className="row py-3 justify-content-center" key={build.id}>
+              <div className="card h-100 border-light bg-transparent">
+                <img
+                  src={build.picture}
+                  className="card-img-top p-3"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title text-info">{build.Name}</h5>
+                  <p className="card-text text-primary">{build.username}</p>
+                  <Link
+                    to={`/builds/detailbuild/${build.id}`}
+                    className="btn btn-outline-primary"
+                  >
+                    Build Detail
+                  </Link>
+                  <div style={{ color: "lightblue" }} className="pt-3">
+                    <i
+                      type="button"
+                      onClick={() => {
+                        deleteBuild(build.id);
+                      }}
+                      className="fa fa-trash fa-xl"
+                      aria-hidden="true"
+                    ></i>
                   </div>
                 </div>
               </div>
-            );
-          })}
-        </div>
-      )}
-      {builds.length === 0 && <h2>No builds available</h2>}
-    </div>
+            </div>
+          </>
+        );
+      } else {
+        return (
+          <div>
+            <h2>No builds available</h2>
+          </div>
+        );
+      }
+    });
+
+  const pageCount = Math.ceil(builds.length / buildsPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+  return (
+    <>
+      <div className="container pt-5 my-5">
+        <div className="row row-cols-3">{displayBuilds}</div>
+      </div>
+      <div className="centerPagination">
+        <ReactPaginate
+          previousLabel={"Previous"}
+          nextLabel={"Next"}
+          pageCount={pageCount}
+          onPageChange={changePage}
+          containerClassName={"paginationBttns"}
+          previousLinkClassName={"previousBttn"}
+          nextLinkClassName={"nextBttn"}
+          disabledClassName={"paginationDisabled"}
+          activeClassName={"paginationActive"}
+        />
+      </div>
+    </>
   );
 }
 export default MyBuilds;
