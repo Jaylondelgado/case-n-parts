63fe39ba73f376a47a9bd187a5e3e77d0311e4d4
Jason <jlee220@gmail.com>
2022-06-24 14:32:56 -0700

    added sql schema and slash connect

---
+++ data/01-create-database.sql
@@ -0,0 +1,3 @@
+CREATE USER great-value WITH SUPERUSER LOGIN PASSWORD 'great-value';
+
+CREATE DATABASE great-value WITH OWNER great-value;
--- data/05-create-review-schema.sql
+++ data/05-create-review-schema.sql
@@ -1,3 +1,5 @@
+\connect great-value
+
 CREATE TABLE IF NOT EXISTS public.rating
 (
     Id SERIAL  PRIMARY KEY,
