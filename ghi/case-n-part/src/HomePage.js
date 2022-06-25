fa3b78c0c133c10e7e1dd00e64d14be9df6e40ac
Jason <jlee220@gmail.com>
2022-06-24 17:44:16 -0700

    removed css error

--- ghi/case-n-part/src/HomePage.js
+++ ghi/case-n-part/src/HomePage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./index.css";
 import "./HomePage.css";
 
 import blackCase from "./images/outer-case/black.png";
